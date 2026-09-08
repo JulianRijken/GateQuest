c25e02004382edfc5317d121967cb61e21cb3b7c
Julian Rijken <mail@julianrijken.com>
2026-09-08 14:35:09 +0200

    Virus update

--- gate_quest_godot/scripts/virus.gd
+++ gate_quest_godot/scripts/virus.gd
@@ -4,22 +4,26 @@ const DUCK = preload("uid://budq36cbadf5k")
 
 func _ready() -> void:
 
-	Engine.time_scale = 3
-		
+	# Engine.time_scale = 0
+
 	var sprites := get_tree().current_scene.find_children("*", "Sprite2D")
 	print("sprites " + str(sprites.size()))
 
 	for sprite : Sprite2D in sprites:
-		# await get_tree().create_timer(0.5).timeout
+		# print(sprite.name + " | " + sprite.get_parent().name + " | " + str(sprite.position) + " | " + str(sprite.texture))
 		sprite.texture = DUCK
 
 	var animatedSprites := get_tree().current_scene.find_children("*", "AnimatedSprite2D")
+	print("animatedSprites " + str(animatedSprites.size()))
 	for animatedSprite : AnimatedSprite2D in animatedSprites:
+		print(animatedSprite.name + " | " + animatedSprite.get_parent().name + " | " + str(animatedSprite.position))
 		animatedSprite.speed_scale = 100
+		animatedSprite.flip_v = true
+
 
 	# Find all tile map layers
-	var tileMaps := get_tree().current_scene.find_children("*", "TileMapLayer")		
-	print("tileMaps " + str(tileMaps.size()))
+	var tileMaps := get_tree().current_scene.find_children("*", "TileMapLayer")	
+	print("tileMaps " + str(tileMaps.size()))	
 
 	# Loop all tileMaps
 	for tileMap : TileMapLayer in tileMaps:
@@ -32,13 +36,18 @@ func _ready() -> void:
 			if source is TileSetAtlasSource:
 				source.texture = DUCK
 
-	# virus_on_death()
+
+	# # Change game over text to quack over
+	# var labels := get_tree().root.find_children("*", "Label")
+	# print("labels: " + str(labels.size()))
+	# for label : Label in labels:
+	# 	print(label.name + " | " + label.get_parent().name)
+
+	print("Going over all nodes")
+	print(get_tree().root.get_child_count())
 
 
-func _process(delta: float) -> void:
-	for nummer in 10:
-		print("Hello i am ducky virus catch me if you can")
-
-
-	
-		# print ("Hello i am ducky virus catch me if you can")
+	var nodes := get_tree().root.find_children("*", "Node")
+	print("nodes: " + str(nodes.size()))
+	for node : Node in nodes:
+		print(node.name + " | " + node.get_parent().name)
